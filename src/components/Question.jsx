6dda8bbdf1f21f7c31e13c85016e954c1cccf9a3
TheoToni <32997269+TheoToni@users.noreply.github.com>
2022-08-30 22:55:13 +0200

    30.08 first logic

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import data from "../data";
 
 function Question() {
+  function checkAnswer() {
+    console.log("checking the answer");
+  }
+
+  const [questions, setQuestions] = React.useState(data);
+  const currentQuestion = questions[0];
+  let counter = 0;
+
   return (
     <div className="questionBox">
-      <h2>What is the earths diameter?</h2>
-      <button>Answer 1</button>
-      <button>Answer 2</button>
-      <button>Answer 3</button>
-      <button>Answer 4</button>
+      <h2>{questions[counter].question}</h2>
+      <button onClick={checkAnswer}>{questions[counter].answer1}</button>
+      <button onClick={checkAnswer}>{questions[counter].answer2}</button>
+      <button onClick={checkAnswer}>{questions[counter].answer3}</button>
+      <button onClick={checkAnswer}>{questions[counter].answer3}</button>
     </div>
   );
 }
